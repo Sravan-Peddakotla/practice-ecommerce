33e6a64a07a3d4aa0c59bdfdc4d82d29c2585534
Sravan-Peddakotla <sravan.peddakotla777@gmail.com>
2024-12-11 14:47:31 +0530

    commit first react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>My first react project</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
